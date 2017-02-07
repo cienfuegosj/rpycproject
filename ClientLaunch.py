41546dc351c70a0b432279bf1cde338912f8956e
Javier Cienfuegos <javier_cienfuegos@berea.edu>
2017-02-07 14:49:35 +0000

    Richard and I completed the Client

--- ClientLaunch.py
+++ ClientLaunch.py
@@ -18,14 +18,39 @@ def main():
       print("Logged in!")
     except ValueError:
       print(ValueError)
+      
+      
+  print ("1 is for registering"
+          "2 is for get quote"
+           "3 is for get quote req name"
+           "4 is for add quote")
     
-  # Register 
+  selection = raw_input("Select an option from above: ")
   
-  if srvclient.register("heannie") == "OK":
-    print("Registration went perfect!")
-  else:
-    print("Could not be registered. Sorry.")
+  # Registration
+  if int(selection) == 1:
+    name = raw_input("Please enter your name: ")
+    if srvclient.register(name) == "OK":
+      print("{0} registered!".format(name))
+    else:
+      print("{0} was not registered!".format(name))
+      
+  # Get Quote
+  elif int(selection) == 2:
+    get_quote = raw_input("Please enter your name: ")
+    print(srvclient.get_quote())
     
-  print(srvclient.get_quote("cienfuegosj"))
+  # Gets Number of Requested Quotes
+  elif int(selection) == 3:
+    get_req_name = raw_input("Please enter your name: ")
+    print("Number of Requested Quote by {0} is {1}".format(get_req_name, srvclient.get_req_name()))
+    
+  # Add Quote
+  elif int(selection) == 4:
+    add_quote = raw_input ("Please enter your name: ")
+    quote = raw_input("Please enter your quote: ")
+    srvclient.add_quote(add_quote, quote)
+    print("Quote added by {0}".format(add_quote))
+    
   
 main()
